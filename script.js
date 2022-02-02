a275c137a8a783ed40b6f45c58cd0f8f4d1fb287
Charles Xavier <charlesxsaldanha@hotmail.com>
2022-02-02 14:46:07 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  // coloque s código aqui
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
